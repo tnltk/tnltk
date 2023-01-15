ad5f1acf9f84513fdfacbd6d039f662de5fb143a
TarikKaanKoc <tarikkaan1koc@gmail.com>
2023-01-15 06:23:29 +0300

    initial commit with project files -r

--- docs/conf.py
+++ docs/conf.py
@@ -25,17 +25,7 @@ version = 'latest'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
-    'sphinx.ext.autosectionlabel'
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
